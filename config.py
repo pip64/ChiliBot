7cfb4eb7f25ca5b3db07ee95cc0f70e6837cd6bb
pip64 <76626915+pip64@users.noreply.github.com>
2022-11-21 21:52:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'NzkzMzUxNDM1MjY1Mzc2MjY3.X-rAGQ._Xoon7IxsZ7PQ4xo1QzzAn742Cs',
+    'token': '',
     'bot': 'ChiliBot',
     'id': '793351435265376267',
     'prefix': 'ChiliBot'
